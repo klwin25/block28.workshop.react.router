35b77da4d31c5a9938565aeafda1106db61cb8bc
Kim <kimnguyentulane@gmail.com>
2024-08-26 01:25:32 -0500

    added code for workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from "react";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
 
 function App() {
-  return <></>;
+  return (
+    <div id="container">
+      {/* <h1>Hello React Router!</h1> */}
+      <div id="navbar">
+        <Link to="/blue">blue</Link>
+        <Link to="/red">red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
